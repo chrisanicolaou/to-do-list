22c5ad87eead476b406338c6d59048e2a3c022e5
chrisanicolaou <chris.anicolaou@yahoo.co.uk>
2022-05-31 17:25:09 +0100

    Basic animation to logo on login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,16 @@ import { useUser } from "../../utils/helpers";
 import LoginForm from "../components/LoginForm";
 import lightLogo from "../assets/logo-light.png";
 import styles from "../App.module.css";
+import { Transition } from "solid-transition-group";
 
 export default function Login() {
   const [user] = useUser();
+
+  const base = { opacity: 1 };
+  const options = { duration: 1000 };
+  const animateIn = (el) => {
+    el.animate([{ opacity: 0 }, base], options);
+  };
 
   if (user) {
     const navigate = useNavigate();
@@ -13,17 +20,19 @@ export default function Login() {
   }
   return (
     <div style={styles.loginContainer}>
-      <img
-        src={lightLogo}
-        height="375px"
-        width="375px"
-        style={{
-          width: "95%",
-          height: "200px",
-          "object-fit": "none",
-          paddingRight: "100px"
-        }}
-      />
+      <Transition onEnter={animateIn} appear={true}>
+        <img
+          src={lightLogo}
+          height="375px"
+          width="375px"
+          style={{
+            width: "95%",
+            height: "200px",
+            "object-fit": "none",
+            paddingRight: "100px"
+          }}
+        />
+      </Transition>
       <LoginForm />
     </div>
   );
