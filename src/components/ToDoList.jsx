a138465c1c8e6113bb00d70bf59d286b38526918
chrisanicolaou <chris.anicolaou@yahoo.co.uk>
2022-06-04 13:23:42 +0100

    Separate ToDoItems into new file

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,9 +3,7 @@ import { createEffect, createResource, createSignal } from "solid-js";
 import { deleteReq, getReq, putReq } from "../../utils/api";
 import { redirect, useUser } from "../../utils/helpers";
 import { useDark } from "./DarkContext";
-import { AiOutlineEdit } from "solid-icons/ai";
-import { TiTickOutline } from "solid-icons/ti";
-import { ImCross } from "solid-icons/im";
+import ToDoItem from "./ToDoItem";
 import {
   DragDropProvider,
   DragDropSensors,
@@ -18,126 +16,6 @@ import {
 const fetchToDos = async () => {
   const [user] = useUser();
   return await getReq(`/todo/${user.email}`);
-};
-
-const ToDoItem = (props) => {
-  const sortable = createSortable(props.toDo.arrayIndex, props.toDo);
-  const [isEditing, setIsEditing] = createSignal(false);
-  const [newDesc, setNewDesc] = createSignal("");
-  const [user] = useUser();
-
-  const openEditMode = () => {
-    setIsEditing(true);
-    setNewDesc(props.toDo.description);
-  };
-
-  const editToDoDescription = async () => {
-    props.setToDos(
-      (toDo) => toDo.toDoId === props.toDo.toDoId,
-      "description",
-      newDesc()
-    );
-    const descToPost = newDesc();
-    setIsEditing(false);
-    setNewDesc("");
-    await putReq(`/todo/${props.toDo.toDoId}`, { description: descToPost });
-  };
-
-  const deleteToDo = async () => {
-    console.log(props.toDos);
-    const updatedToDos = [...props.toDos];
-    const index = updatedToDos.findIndex(
-      (toDo) => toDo.toDoId === props.toDo.toDoId
-    );
-    updatedToDos.splice(index, 1);
-    props.setToDos(updatedToDos);
-    await deleteReq(`/todo/${props.toDo.toDoId}/t/t`);
-  };
-
-  return (
-    // <div
-    //   use:sortable
-    //   class="sortable"
-    //   classList={{ "opacity-25": sortable.isActiveDraggable }}
-    // >
-    //   {props.toDo.description}
-    // </div>
-    <div
-      use:sortable
-      class="sortable"
-      classList={{ "opacity-25": sortable.isActiveDraggable }}
-    >
-      <ListGroupItem
-        as="li"
-        action
-        style={{ display: "flex", "align-content": "center" }}
-      >
-        {isEditing() ? (
-          <>
-            <Form.Control
-              onChange={(e) => setNewDesc(e.target.value)}
-              value={newDesc()}
-            />
-            <Button
-              onClick={editToDoDescription}
-              variant="primary"
-              size="sm"
-              style={{
-                "margin-left": "10px",
-                "align-self": "center"
-              }}
-            >
-              <TiTickOutline size={30} color="#000000" />
-            </Button>
-          </>
-        ) : (
-          <>
-            <div
-              onClick={() =>
-                updateToDoActive(
-                  props.toDo,
-                  [props.index()],
-                  !props.toDo.isActive,
-                  props.setToDos
-                )
-              }
-            >
-              {props.toDo.isActive ? (
-                props.toDo.description
-              ) : (
-                <strike>{props.toDo.description}</strike>
-              )}
-            </div>
-            <Button
-              onClick={openEditMode}
-              variant="primary"
-              size="sm"
-              style={{
-                padding: "1px",
-                "padding-bottom": "2px",
-                "margin-left": "10px",
-                "align-self": "center"
-              }}
-            >
-              <AiOutlineEdit size={20} color="#000000" />
-            </Button>
-            <Button
-              onClick={deleteToDo}
-              variant="danger"
-              size="sm"
-              style={{
-                padding: "3px",
-                "margin-left": "10px",
-                "align-self": "center"
-              }}
-            >
-              <ImCross size={20} color="#FFFFFF" />
-            </Button>
-          </>
-        )}
-      </ListGroupItem>
-    </div>
-  );
 };
 
 const updateToDoActive = async (toDo, index, value, setToDos) => {
